47864afca1a62c7843968a20f766e5d65b48f970
Shwshank <shwshank3@gmail.com>
2020-10-18 09:44:35 +0530

    cicd test1 build

---
+++ build/precache-manifest.ad36f720ca623d95d8816c7e1429267b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0eab2d50f79b19021839ad08c055a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef2eb5ae2891a5cc8310",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7b94170fc85e2e70f035",
+    "url": "/static/css/main.b243cba3.chunk.css"
+  },
+  {
+    "revision": "ef2eb5ae2891a5cc8310",
+    "url": "/static/js/2.92ed3c6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.92ed3c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b94170fc85e2e70f035",
+    "url": "/static/js/main.837250f5.chunk.js"
+  },
+  {
+    "revision": "9575ae87e805f57f6f08",
+    "url": "/static/js/runtime-main.2aea7f39.js"
+  }
+]);
